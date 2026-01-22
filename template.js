27b3d75fa8ad74c67b4b99813067e3c4446817c2
Jorens <jorensmerenjanu@gmail.com>
2026-01-22 03:12:30 +0200

    fix: 3+ level nested properties not being parsed

--- template.js
+++ template.js
@@ -8,9 +8,10 @@ function localizeString(str, locale, path = "") {
     let parsedStr = str;
     for(key in locale) {
         if(typeof locale[key] === 'string') {
+            console.log(path + key);
             parsedStr = parsedStr.replaceAll("{{" + path + key + "}}", locale[key]);
         } else if (typeof locale[key] === 'object') {
-            parsedStr = localizeString(parsedStr, locale[key], key + ".");
+            parsedStr = localizeString(parsedStr, locale[key], path + key + ".");
         }
     }
     return parsedStr;
